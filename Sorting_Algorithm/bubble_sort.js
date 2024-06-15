5da3f0e9cd33ce2d1de6435ca1f8b0a9ffad6f56
r.gedhar007@gmail.com <Ravi@#9767@#>
2024-06-15 23:01:53 +0530

    bubble_sort

---
+++ Sorting_Algorithm/bubble_sort.js
@@ -0,0 +1,25 @@
+// bubble sort: - 
+
+// algorithm that sorts an array from the lowest value to the highest value 
+
+// time complexity :- O(n*n)
+
+let arr = [3, 11, 34, 333, 56, 23, 1, 33, 78, 0];
+function bubble_Sort(arr) {
+    let a = b = c = null;
+    let i = 0;
+    let n = 0;
+    for (i = 0; i < arr.length; i++){
+        for (n = 0; n < arr.length; n++){
+            a = n; b = n + 1;
+            if (arr[a] > arr[b]) {
+                let c = arr[a];
+                arr[a] = arr[b]
+                arr[b]=c
+            }
+        }
+    }
+    console.log(arr)
+}
+
+bubble_Sort(arr)
